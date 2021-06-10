7110eb4ee88093e5699e3947f5f250f685225828
TimmyNic <tim.nicolas@outlook.com>
2021-06-10 16:06:05 +1200

    Reformatted code.

--- mapclientplugins/simulationtask2step/view/simulationtask2widget.py
+++ mapclientplugins/simulationtask2step/view/simulationtask2widget.py
@@ -8,6 +8,7 @@ from PySide2 import QtCore, QtWidgets
 import numpy as np
 
 import matplotlib
+
 # matplotlib.use('Qt4Agg')
 # matplotlib.rcParams['backend.qt4']='PySide'
 matplotlib.use('Qt5Agg')
@@ -25,11 +26,11 @@ from matplotlib.backend_bases import key_press_handler
 from mapclientplugins.simulationtask2step.view.ui_simulationtask2widget import Ui_SimulationTask2Widget
 from mapclientplugins.simulationtask2step.sedml.execute import ExecuteSedml
 
+
 class SimulationTask2Widget(QtWidgets.QWidget):
     '''
     classdocs
     '''
-
 
     def __init__(self, parent=None):
         '''
@@ -52,7 +53,7 @@ class SimulationTask2Widget(QtWidgets.QWidget):
         vbox.addWidget(self.canvas)  # the matplotlib canvas
         vbox.addWidget(self.mpl_toolbar)
         self._ui.plotPane.setLayout(vbox)
-        #self.setCentralWidget(self.main_frame)
+        # self.setCentralWidget(self.main_frame)
 
         self.createAxes()
         self._makeConnections()
@@ -61,18 +62,18 @@ class SimulationTask2Widget(QtWidgets.QWidget):
         self.axes = self.fig.add_subplot(111)
         self.drawSineFunction()
         self.canvas.draw()
-        
+
     def setSimulationRoot(self, location):
         self.sedml.setSimulationRoot(location)
-        
+
     def _makeConnections(self):
         self._ui.doneButton.clicked.connect(self._doneButtonClicked)
         self._ui.simulateButton.clicked.connect(self._simulateButtonClicked)
         self._ui.clearButton.clicked.connect(self._clearButtonClicked)
-        
+
     def drawSineFunction(self):
         # draw sine function
-        t = np.arange(0.0, 2.0*np.pi, 0.01)
+        t = np.arange(0.0, 2.0 * np.pi, 0.01)
         s = np.sin(t)
         self.axes.plot(t, s, label="sin(t)")
 
@@ -93,28 +94,26 @@ class SimulationTask2Widget(QtWidgets.QWidget):
         results = self.sedml.execute(h, n, methodId)
         if results == None:
             return
-        #print results	
-        #print data
-        #print data.shape
-        #print data.dtype.names
-        #print data['X']
-        #self.axes.plot(data['X'], data['sinX'], label='sin(x)')
+        # print results
+        # print data
+        # print data.shape
+        # print data.dtype.names
+        # print data['X']
+        # self.axes.plot(data['X'], data['sinX'], label='sin(x)')
         title = "h=" + str(h) + "; " + methodLabel + "; time=" + str(results['time'])
         self.axes.plot(results['data']['X'], results['data']['Derivative_approximation'], marker="o", label=title)
         self.axes.legend()
         self.canvas.draw()
-    
+
     def _clearButtonClicked(self):
         self.fig.clear()
         self.createAxes()
-                        
+
     def initialise(self):
         print("Initialise called?")
-        
+
     def registerDoneExecution(self, callback):
         self._callback = callback
-        
+
     def _doneButtonClicked(self):
         self._callback()
-        
-
