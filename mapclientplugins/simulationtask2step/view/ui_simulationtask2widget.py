5e27e4d75aac634cc1710ea1a01aeebaef9e751f
TimmyNic <tim.nicolas@outlook.com>
2023-01-19 17:00:28 +1300

    Upgrade to PySide6.

--- mapclientplugins/simulationtask2step/view/ui_simulationtask2widget.py
+++ mapclientplugins/simulationtask2step/view/ui_simulationtask2widget.py
@@ -3,15 +3,22 @@
 ################################################################################
 ## Form generated from reading UI file 'simulationtask2widget.ui'
 ##
-## Created by: Qt User Interface Compiler version 5.15.2
+## Created by: Qt User Interface Compiler version 6.4.1
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide2.QtCore import *
-from PySide2.QtGui import *
-from PySide2.QtWidgets import *
-
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QDockWidget, QDoubleSpinBox, QGroupBox,
+    QHBoxLayout, QLabel, QPushButton, QRadioButton,
+    QSizePolicy, QSpacerItem, QSpinBox, QVBoxLayout,
+    QWidget)
 
 class Ui_SimulationTask2Widget(object):
     def setupUi(self, SimulationTask2Widget):
